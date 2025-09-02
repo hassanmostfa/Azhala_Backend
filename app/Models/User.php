2a9b2dfab484ed7981a3855b8d8f88f2c7b679e4
Hassan Mostafa <01092841138>
2025-09-02 12:07:04 +0300

    Test Api Branch Commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,6 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'phone_code',
